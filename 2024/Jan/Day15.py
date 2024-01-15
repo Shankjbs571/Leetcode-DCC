ad7f34933a21cbf48fa78b609a99cbcebd5d5849
Shashank Kumar <shankjbs571@gmail.com>
2024-01-15 22:25:53 +0530

    Day15

---
+++ 2024/Jan/Day15.py
@@ -0,0 +1,29 @@
+#📚 Problem Sources:
+# Leetcode:
+# https://leetcode.com/problems/find-players-with-zero-or-one-losses/?envType=daily-question&envId=2024-01-15
+
+#S O L U T I O N
+
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        zero_loss = set()
+        one_loss = set()
+        more_losses = set()
+        
+        for winner, loser in matches:
+            # Add winner
+            if (winner not in one_loss) and (winner not in more_losses):
+                zero_loss.add(winner)
+            # Add or move loser.
+            if loser in zero_loss:
+                zero_loss.remove(loser)
+                one_loss.add(loser)
+            elif loser in one_loss:
+                one_loss.remove(loser)
+                more_losses.add(loser)
+            elif loser in more_losses:
+                continue
+            else:
+                one_loss.add(loser)          
+            
+        return [sorted(list(zero_loss)), sorted(list(one_loss))]
