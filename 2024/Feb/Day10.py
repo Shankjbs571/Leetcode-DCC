ed043dd69ad098809a709be8c0c7fb0ea0c04965
Shashank Kumar <shankjbs571@gmail.com>
2024-02-10 21:31:45 +0530

    Day10

---
+++ 2024/Feb/Day10.py
@@ -0,0 +1,23 @@
+#📚 Problem Sources:
+# Leetcode:
+# https://leetcode.com/problems/palindromic-substrings/description/?envType=daily-question&envId=2024-02-10
+
+# S O L U T I O N
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        n, ans = len(s), 0
+        
+        def palindromeCount(left: int, right: int) -> int:
+            count = 0
+            while left >= 0 and right < n and s[left] == s[right]:
+                left -= 1
+                right += 1
+                count += 1
+            return count
+        
+        for i in range(n):
+            even = palindromeCount(i, i + 1)
+            odd = palindromeCount(i, i)
+            ans += even + odd
+            
+        return ans
