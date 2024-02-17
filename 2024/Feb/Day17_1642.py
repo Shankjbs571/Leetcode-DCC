22e4b584617f555bc97b7d3e7bb1f40ed93b89a2
Shashank Kumar <shankjbs571@gmail.com>
2024-02-17 18:05:45 +0530

    1642. Furthest Building You Can Reach

---
+++ 2024/Feb/Day17_1642.py
@@ -0,0 +1,19 @@
+#📚 Problem Sources:
+# Leetcode:https://leetcode.com/problems/furthest-building-you-can-reach/description/?envType=daily-question&envId=2024-02-17
+
+#1642. Furthest Building You Can Reach
+
+
+# S O L U T I O N
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        heap = []
+        for i in range(len(heights) - 1):
+            diff = heights[i + 1] - heights[i]
+            if diff > 0:
+                heapq.heappush(heap, diff)
+                if len(heap) > ladders:
+                    bricks -= heapq.heappop(heap)
+                if bricks < 0:
+                    return i
+        return len(heights) - 1
